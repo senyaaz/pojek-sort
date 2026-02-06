4ce91e32df8afb161ce5ea76e96c49903576d70e
sitnikovs <ssitnikov304@gmail.com>
2026-02-06 13:20:07 +0700

    sitnikovs

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,1 +1,16 @@
-# 
+def split_array(arr):
+    mid = len(arr) // 2
+    return arr[:mid], arr[mid:]
+
+def analyze_half(half, name="Половина"):
+    if not half:
+        return {"name": name, "size": 0, "sum": 0, "avg": 0}
+    
+    return {
+        "name": name,
+        "size": len(half),
+        "sum": sum(half),
+        "avg": sum(half) / len(half),
+        "min": min(half),
+        "max": max(half)
+    }
