252de8b4f44407020793cba423a20426c80e363e
senyaaz <semensmirnov4730@gmail.com>
2026-02-06 13:10:56 +0700

    senyaaz

--- src/sorter.py
+++ src/sorter.py
@@ -1,1 +1,16 @@
-# 
+def quick_sort(arr):
+    """Быстрая сортировка массива."""
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]  
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    
+    return quick_sort(left) + middle + quick_sort(right)
+
+def generate_array(size=10):
+    """Генерирует случайный массив."""
+    import random
+    return [random.randint(1, 100) for _ in range(size)]
